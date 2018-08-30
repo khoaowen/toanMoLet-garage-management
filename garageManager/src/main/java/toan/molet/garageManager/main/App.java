83c215c1130370623868d3105a89cdea29f96a31
khoa <khoa@DESKTOP-531D76G>
2018-08-30 22:58:27 +0200

    First commit

---
+++ garageManager/src/main/java/toan/molet/garageManager/main/App.java
@@ -0,0 +1,19 @@
+package toan.molet.garageManager.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("JavaFX Welcome");
+
+		primaryStage.show();
+	}
+}
